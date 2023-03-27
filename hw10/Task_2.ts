f26a743b62c2ba4ab788d8ae48c1f4fe8468f077
Daria <daboris91@gmail.com>
2023-03-27 17:30:26 +0300

    Add Task_2.ts, Task_3.ts

--- hw10/Task_2.ts
+++ hw10/Task_2.ts
@@ -1,3 +1,18 @@
 // Task 2
 // Создайте два объекта людей. Реализуйте метод, который выводит строку `My name is <name>`.
 // И в зависимости от контекста выполнения, этот метод должен выводить соответвующее имя
+
+const human1: {name: string} = {
+    name: 'Vasya',
+};
+
+const human2: {name: string} = {
+    name: 'Petya',
+};
+
+function sayMyName(this: {name: string}) {
+    console.log(`My name is ${this.name}.`);
+}
+
+sayMyName.call(human1)
+sayMyName.call(human2)
--- hw10/Task_3.ts
+++ hw10/Task_3.ts
@@ -2,3 +2,20 @@
 // Создайте два объекта машин. Реализуйте метод выводящий количество дверей машины, и с какой стороны находится руль.
 // В зависимости от контекста выполнения, этот метод должен выводить соответвующую информацию о машинах.
 // К примеру `This car has 3 doors and this is left-hand drive car`
+
+const car1 = {
+    numOfDoors: 4,
+    steeringWheelSide: 'left-hand',
+};
+
+const car2 = {
+    numOfDoors: 6,
+    steeringWheelSide: 'right-hand',
+};
+
+function giveCarInfo (this: any) {
+    console.log(`This car has ${this.numOfDoors} doors and this is a ${this.steeringWheelSide} drive car.`)
+}
+
+giveCarInfo.call(car1)
+giveCarInfo.call(car2)
